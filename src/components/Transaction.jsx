8ccfc1b72c67353296ba77e178633087e596527e
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-09-24 22:35:37 +0530

    chnages

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -4,10 +4,14 @@ import transactionEnum from "../utils/enums/transactionEnum";
 export default function Transaction(props) {
 	const { amount, type, comment, tag, date } = props;
 
+	function isCredit() {
+		return type == transactionEnum.CREDIT;
+	}
+
 	return (
 		<div style={{ margin: ".5rem", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
 			<span style={{ flex: 2 }}>{comment}</span>
-			<span style={{ flex: 2 }}>{`${type == transactionEnum.DEBIT ? "-" : "+"}${amount}`}</span>
+			<span style={{ flex: 2, fontWeight: "bold", color: isCredit() ? "darkgreen" : "darkred" }}>{`${!isCredit() ? "-" : "+"}${amount}`}</span>
 			<span style={{ flex: 1 }}>{moment(date).fromNow()}</span>
 		</div>
 	);
