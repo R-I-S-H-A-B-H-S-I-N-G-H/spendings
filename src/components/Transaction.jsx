ca2585487108935c2db1fc3303959d7093564b97
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-10-06 01:38:03 +0530

    transaction style changes

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -10,12 +10,12 @@ export default function Transaction(props) {
 	}
 
 	return (
-		<div style={{ marginTop: ".5rem", display: "flex", flexDirection: "row", alignItems: "center", justifyItems: "center", gap: "5px" }}>
+		<div style={{ marginTop: ".5rem", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", gap: "5px" }}>
 			<Text size="2" style={{ flex: 1.5 }}>
 				{comment}
 			</Text>
-			<Text size="2" weight={"bold"} color={isCredit() ? "green" : "red"} style={{ flex: 2 }}>{`${!isCredit() ? "-" : "+"}${amount}`}</Text>
-			<Text size="2" style={{ flex: 3, padding: "0rem .5rem" }}>
+			<Text align={"center"} size="2" weight={"bold"} color={isCredit() ? "green" : "red"} style={{ flex: 2 }}>{`${!isCredit() ? "-" : "+"}${amount}`}</Text>
+			<Text size="2" style={{ padding: "0rem .5rem" }}>
 				{moment(date).fromNow()}
 			</Text>
 			<Button size="1" variant="outline" color="red" onClick={() => onTransactionDel(id)}>
