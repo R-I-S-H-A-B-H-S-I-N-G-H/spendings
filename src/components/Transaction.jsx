20c26b64102004892f75b2841f1da86c023f8df5
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-10-19 01:25:16 +0530

    fixed date visuals

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -9,6 +9,13 @@ export default function Transaction(props) {
 		return type == transactionEnum.CREDIT;
 	}
 
+	function isDateToday(dateStr) {
+		if (dateStr == null) return false;
+		const date = new Date(dateStr);
+		const todayDate = new Date();
+		return todayDate.getDate() == date.getDate() && todayDate.getMonth() == date.getMonth() && todayDate.getFullYear() == date.getFullYear();
+	}
+
 	return (
 		<div style={{ marginTop: ".5rem", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", gap: "5px" }}>
 			<Text size="2" style={{ flex: 1.5 }}>
@@ -16,7 +23,7 @@ export default function Transaction(props) {
 			</Text>
 			<Text align={"center"} size="2" weight={"bold"} color={isCredit() ? "green" : "red"} style={{ flex: 2 }}>{`${!isCredit() ? "-" : "+"}${amount}`}</Text>
 			<Text size="2" style={{ padding: "0rem .5rem" }}>
-				{moment(date).format("DD/MM/YYYY")}
+				{isDateToday(date) ? moment(date).fromNow() : moment(date).format("Do MMM YY")}
 			</Text>
 			<Button size="1" variant="outline" color="red" onClick={() => onTransactionDel(id)}>
 				{"-"}
